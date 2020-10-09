f5543c233c14d02e72e44dfe7313fddd3e59597f
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-09 16:34:13 +0900

    protospaceの変更、修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -45,15 +46,18 @@ class PrototypesController < ApplicationController
     @comments = @prototype.comments.includes(:user)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+
 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
   end
 
 end
