16a96063b8490505fc55ecf6ab2f892d924885ec
daichi-code <daichi.pr.daichi@gmail.com>
2020-10-09 12:31:08 +0900

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  #before_action :authenticate_user! only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+    @name = @user.name
+    @prototype = @user.prototypes
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile, :occupation, :position).merge(user_id: current_user.id)
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
